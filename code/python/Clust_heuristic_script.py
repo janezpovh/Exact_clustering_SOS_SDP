da51c519cd732eb5bc617245644feb6cd28189e9
Shudian Zhao <shudianz@outlook.com>
2022-10-20 20:24:31 +0200

    add metrics for k_means and summarise in table

--- code/python/Clust_heuristic_script.py
+++ code/python/Clust_heuristic_script.py
@@ -52,26 +52,105 @@ def clustering_heu_method(results_folder,data_folder,dataname,heu):
 
     # breakpoint()
     cluster_table.to_csv(results_path)
-    # with open(results_path,"w") as outfile:
-    #     writer =csv.writer(outfile)
-    #     key_list=list(cluster_dict.keys())
-    #     writer.writerow(cluster_dict.keys())
-    #     writer.writerow([cluster_dict[x] for x in key_list])
+
+
+def clustering_metrics_true_labels(data_folder,clustering_folder,evaluation_folder,dataname):
+    data_label_path = data_folder + dataname + '_with_clust_labels.txt'
+    clustering_path = clustering_folder + dataname +'_k_means_clust_labels.csv'
+    eval_path = evaluation_folder + dataname+'_k_means_clust_eval_with_true_label.csv'
+
+    if isfile(clustering_path) and ~isfile(eval_path):
+        labels_pred_table=pd.read_csv(clustering_path)
+        klist = labels_pred_table.columns[2:]
+        # breakpoint()
+        n,d,X,labels_true = read_sample_labels(data_label_path)
+
+        true_k = len(set(labels_true))
+
+        metric_scores_summary=pd.DataFrame()
+        metric_scores_summary['k']= np.append(klist,true_k)
+
+        metrics_list_extrin= [normalized_mutual_info_score,
+        mutual_info_score,homogeneity_score,fowlkes_mallows_score,
+        # cluster.contingency_matrix, cluster.pair_confusion_matrix, v_measure_score,
+        completeness_score, adjusted_rand_score,adjusted_mutual_info_score]
+
+        metrics_list_intrin = [calinski_harabasz_score,davies_bouldin_score,silhouette_score]
+
+		# if_k_true = []
+        for func in metrics_list_extrin:
+            metric_scores =[]
+            for k in klist:
+
+                labels_pred = labels_pred_table[k]
+                print("Preidcted labesl view:.{}\n".format(labels_pred[:10]))
+                print("True labesl view:.{}\n".format(labels_true[:10]))
+                # print("Diff counts:{}\n".format(sum( x!=int(y) for x, y in zip(labels_pred,labels_true))))
+                metric_scores.append(func(labels_true,labels_pred))
+
+        #  add row for the ground true label
+            metric_scores.append(func(labels_true,labels_true))
+
+            metric_scores_summary[func.__name__]=metric_scores
+
+        for func in metrics_list_intrin:
+            metric_scores =[]
+
+            for k in klist:
+
+            	labels_pred = labels_pred_table[k]
+            	# print(labels_pred)
+            	# print(labels_true)
+            	metric_scores.append(func(X,labels_pred))
+
+        #  add row for the ground true label
+            metric_scores.append(func(X,labels_true))
+            metric_scores_summary[func.__name__]=metric_scores
+
+        metric_scores_summary.to_csv(eval_path)
+
+def combinefile(datafolderpath,filefolderpath,combinedpath):
+	data_list = [f.split(".")[0] for f in listdir(datafolderpath)
+	if isfile(filefolderpath+f.split(".")[0]+"_k_means_clust_eval_with_true_label.csv") and  "_k_means_clust_labels" not in f ]
+
+	print(data_list)
+	# newpd= pd.read_csv(filefolderpath+data_list[0]+"_clust_eval.csv")
+
+	combined_csv = pd.concat([pd.read_csv(filefolderpath+f+"_k_means_clust_eval_with_true_label.csv",index_col=0).assign(dataname=f)
+		for f in data_list])
+
+	combined_csv_firstcol= combined_csv.pop('dataname')
+	combined_csv.insert(0,'dataname',combined_csv_firstcol)
+
+	summary_csv = combined_csv.sort_values(by=['dataname','k'])
+
+
+
+
+	summary_csv.to_csv(combinedpath, index=False, encoding='utf-8-sig')
+
+
+
+
 
 def main():
-    data_folder='/Users/shzhao/Documents/GitHub/Exact_clustering_SOS_SDP/input_data_for_exact_solver/artificial_data/'
+    data_folder='/Users/shzhao/Documents/GitHub/Exact_clustering_SOS_SDP/input_data_for_exact_solver/real_data/'
     results_folder='/Users/shzhao/Documents/GitHub/Exact_clustering_SOS_SDP/Results/Results_clustering/Other_Clustering/'
+    evaluation_folder='/Users/shzhao/Documents/GitHub/Exact_clustering_SOS_SDP/Results/Results_quality_of_clusters/other_methods'
+    summaryfile = '/Users/shzhao/Documents/GitHub/Exact_clustering_SOS_SDP/Results/Results_quality_of_clusters/other_methods/real_data_summary_all_label_clust_eval.csv'
+
+
     dataname_real_list=['dermatology','ecoli','glass','haberman','heart-statlog','iono','iris','sonar','tae',
     'thy','wine','wisc','zoo']
     dataname_artficial_list=['3-spiral','3MC','blobs','flame','gaussians1','insect',
     'jain','lsun','pathbased','zelnik1','zelnik2','zelnik6']
-    # dataname_list = [f.split(".")[0] for f in listdir(data_folder)
-	# if isfile(join(data_folder, f)) and "_with_clust_labels" not in f]
     heu_list=[k_means]
 
-    for data in dataname_artficial_list:
-        for heu in heu_list:
-            clustering_heu_method(results_folder,data_folder,data,heu)
+    for data in dataname_real_list:
+        # for heu in heu_list:
+            # clustering_heu_method(results_folder,data_folder,data,heu)
+        clustering_metrics_true_labels(data_folder,results_folder,evaluation_folder,data)
+    combinefile(data_folder,evaluation_folder,summaryfile)
 
 
 if __name__=='__main__':
